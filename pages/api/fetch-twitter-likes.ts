c8144c5d3e8d5d99035f270d374be8c1411432ca
Ray Louis <x1337c@gmail.com>
2022-08-12 23:15:26 -0700

    ????

--- pages/api/fetch-twitter-likes.ts
+++ pages/api/fetch-twitter-likes.ts
@@ -23,13 +23,11 @@ type TwitterLikeObject = {
 
 async function twitterFetchLoop(pages: Number, fetchInfo: { bearerToken?: string, endpoint: string }) {
   let { endpoint, bearerToken } = fetchInfo
+  let tokenWeLeftOffOn: string = ""
   let paginationTokenQueryKeyAndValue: string = ""
   let allLikes: Array<TwitterLikeObject> = []
 
   for (let i = 0; i < pages; i++) {
-    if (paginationTokenQueryKeyAndValue === "naw we done") {
-      break
-    }
     let completeEndpoint = endpoint + paginationTokenQueryKeyAndValue
     try {
       let response = await fetch(completeEndpoint, {
@@ -40,16 +38,13 @@ async function twitterFetchLoop(pages: Number, fetchInfo: { bearerToken?: string
       })
       let responseJson: TwitterResponse = await response.json()
       allLikes.push(...responseJson.data)
-      if (responseJson.meta.next_token) {
-        paginationTokenQueryKeyAndValue = `&pagination_token=${responseJson.meta.next_token}`
-      } else {
-        paginationTokenQueryKeyAndValue = "naw we done"
-      }
+      paginationTokenQueryKeyAndValue = `&pagination_token=${responseJson.meta.next_token}`
+      tokenWeLeftOffOn = responseJson.meta.next_token
     } catch (error) {
       console.error(error)
     }
   }
-  return allLikes
+  return {allLikes, tokenWeLeftOffOn}
 }
 
 // let's map out how this is going to work. first i make the first request to get the first set of liked tweets. from that response i get the next token for the next page. i think i'll set a variable to &pagination_token plus the token and change it every iteration. i'll append that string to the end of the endpoint and shoot out the request to twitter. that ought to do it. 
@@ -70,7 +65,8 @@ export default async function handler(
   }
 
   const query: FetchLikesQuery = req.query
-  const pages_to_fetch = Number(query.pages_to_fetch)
+  // const pages_to_fetch = Number(query.pages_to_fetch)
+  const pages_to_fetch = 10
 
   const yeet = await twitterFetchLoop(pages_to_fetch, fetchInfo)
 
@@ -81,27 +77,27 @@ export default async function handler(
   // } catch (error) {
   //   console.error(error)
   // }
-  
+
   let yeetString2 = "{"
-  for (let i = 0; i < yeet.length; i++) {
-    yeetString2 += `"${String(i)}":` + JSON.stringify(yeet[i])
-    if (i !== yeet.length - 1) {
+  for (let i = 0; i < yeet.allLikes.length; i++) {
+    yeetString2 += `"${String(i)}":` + JSON.stringify(yeet.allLikes[i])
+    if (i !== yeet.allLikes.length - 1) {
       yeetString2 += ", \n"
     }
   }
-  yeetString2 += "}"
+  yeetString2 += `, \n "next_token": "${yeet.tokenWeLeftOffOn}"}`
   // for (let i = 0; i < yeet.length; i++) {
   //   yeetString2 += `{"id": "${String(yeet[i].id)}", "created_at": "${String(yeet[i].created_at)}", "text": "${String(yeet[i].text)}"}\n`
   // }
 
   try {
-    fs.writeFileSync(pathToFile + "/likes.json", yeetString2)
+    fs.writeFileSync(pathToFile + "/likesnew.json", yeetString2)
   } catch (error) {
     console.error(error)
   }
 
 
-  
-  res.status(200).json({ ...yeet })
+
+  res.status(200).json({ ...yeet.allLikes })
 
 }
