a1b7f2b256aea71185f6c6635e936bf5fe2a0097
Ray Louis <x1337c@gmail.com>
2022-08-13 21:43:48 -0700

    syke i'm back on it. but i'm leaving now for real because i don't feel so good. got axes to appear. hella ugly though

--- components/Chart1.jsx
+++ components/Chart1.jsx
@@ -9,11 +9,12 @@ function BarChart() {
   const graphRef = useRef(null)
 
   function drawChart() {
+
     let bruh = d3.select(graphRef.current)
       .append("svg")
       .attr("width", 500)
       .attr("height", 500)
-      .style("background-color", "#cccccc")
+      .style("background-color", "#ffffff")
       .style("padding", 10)
 
     bruh.selectAll("rect")
@@ -25,10 +26,26 @@ function BarChart() {
       .attr("width", 65)
       .attr("height", (d, i) => d * 10)
       .attr("fill", "red")
+
+    const x = d3.scaleLinear()
+      .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .range([0, 400]);
+    bruh.append("g")
+      .attr("transform", `translate(0, 400)`)
+      .attr("color", "blue")
+      .call(d3.axisBottom(x));
+
+    const y = d3.scaleLinear()
+      .domain([0, 100])
+      .range([1000, 0])
+    bruh.append("g")
+      .attr("transform", `translate(50, 0)`)
+      .attr("color", "blue")
+      .call(d3.axisLeft(y))
   }
 
   return (
-    <div ref={graphRef} className={styles.graph}/>
+    <div ref={graphRef} className={styles.graph} />
   )
 }
 
