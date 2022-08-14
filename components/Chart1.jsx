bd3fe54ede0def4e3e3e8d49e4e1d7aa3ebffe65
Ray Louis <x1337c@gmail.com>
2022-08-13 19:47:52 -0700

    super simple d3 graph appears. but it appears twice all the time. wonder why.

--- components/Chart1.jsx
+++ components/Chart1.jsx
@@ -1,11 +1,36 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
+import * as d3 from "d3"
 
 function BarChart () {
+  useEffect (() => {
+    drawChart()
+  }, [])
   const graphRef = useRef(null)
+
+  function drawChart() {
+    let bruh = d3.select(graphRef.current)
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500)
+    .style("background-color", "#cccccc")
+    .style("padding", 10)
+
+    bruh.selectAll("rect")
+    .data([12,24,54,3,42,33,60,14])
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i * 70)
+    .attr("y", (d, i) => 500 - 10 * d)
+    .attr("width", 65)
+    .attr("height", (d,i) => d * 10)
+    .attr("fill", "red")
+  }
+  
   return (
-    <div ref={graphRef}>
-      sup
-    </div>
+    <>
+      <div ref={graphRef} />
+      a
+    </>
   )
 }
 
