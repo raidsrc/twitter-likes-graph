0f2400b7d2fa3f539aa20bd65148f119e3bcadc5
Ray Louis <x1337c@gmail.com>
2022-08-17 21:14:19 -0700

    first official build

--- components/realBarChart.tsx
+++
@@ -1,9 +0,0 @@
-import { VictoryBar } from "victory"
-
-const data = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
-
