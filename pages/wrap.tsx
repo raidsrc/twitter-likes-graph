ab129bb93736cb0735d39ede2202601df6ef41dc
Ray Louis <x1337c@gmail.com>
2022-08-17 21:32:27 -0700

    some more writing

--- pages/wrap.tsx
+++ pages/wrap.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
           <h1>Wrap-up</h1>
           <div className={styles.textBlurb}>
             <p>{`Well that was a fun exercise in web programming, using the Twitter API, ETL, and data visualization. I've never wielded data on this scale before. Almost 20k tweets. Wow. That's a lot more data than I'm used to handling.`}</p>
-            <p>{`I still have COVID-19. Tested positive earlier today. It's been more than a week since I first started showing symptoms and I'm still suffering. My throat's doing better but I'm still feeling drowsy and not all there in the head. So I'm gonna go back to sleep. Hope you enjoyed taking a tour through this web app. Peace.`}</p>
+            <p>{`I still have COVID-19. Tested positive earlier today. It's been more than a week since I first started showing symptoms and I'm still suffering. My throat's doing better but I'm still feeling drowsy and not all there in the head. I feel like my brain isn't functioning at full capacity. Like I'm drunk. Even though I haven't had a single microliter of alcohol recently. So I'm gonna go back to resting. Hope you enjoyed taking a tour through this web app. Peace.`}</p>
           </div>
 
         </div>
